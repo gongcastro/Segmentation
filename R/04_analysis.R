944363a8bd720ec51e4d29317b23b72a75fde348
gongcastro <gonzalo.garciadecastro@upf.edu>
2019-10-22 12:32:41 +0200

    Create TOST plot and add vowel distribution plot to manuscript

--- R/04_analysis.R
+++ R/04_analysis.R
@@ -5,6 +5,7 @@
 #### set up #############################################
 
 # load packages
+library(here)     # for locating files
 library(magrittr) # for working with pipes
 library(dplyr)    # for manipulating data
 library(purrr)    # for working with lists
@@ -13,6 +14,7 @@ library(tibble)   # for tidy data presentation
 library(lme4)     # for LMEM
 library(car)      # fir checking assumptions
 library(TOSTER)   # for equivalence testing
+library(ggplot2)  # for visualising data
 
 # set statistical parameters
 alpha <- 0.05 # significance criterion
@@ -22,10 +24,10 @@ sesoi <- 0.50 # smallest effect size of interest (SESOI)
 #### import data ##############################################
 
 # participant-level long data ready for main analysis
-data <- read.delim('Data/02_aggregated.txt') %>% # wide data
+data <- read.delim(here("Data", "02_aggregated.txt")) %>% # wide data
   as_tibble() %>%
   select(participant, familiar, novel) %>%
-  gather(key = 'trial_type', value = 'time', -participant) %>%
+  gather(key = "trial_type", value = "time", -participant) %>%
   mutate(trial_type = as.factor(trial_type))
 
 #### significance testing #####################################
@@ -35,7 +37,7 @@ tost <- data %>%
   spread(trial_type, time) %>%
   dataTOSTpaired(
     data         = .,
-    pairs        = list(c(i1 = 'familiar', i2 = 'novel')),
+    pairs        = list(c(i1 = "familiar", i2 = "novel")),
     low_eqbound  = -sesoi,
     high_eqbound = sesoi,
     alpha        = alpha,
@@ -50,6 +52,32 @@ tost.table <- data.frame(
   p    = c(tost$tost$asDF$`p[0]`, tost$tost$asDF$`p[1]`, tost$tost$asDF$`p[2]`),
   d    = c(tost$tost$asDF$`t[0]`/sqrt(n), tost$tost$asDF$`t[1]`/sqrt(n), tost$tost$asDF$`t[2]`/sqrt(n))
 )
+
+# plot tost
+ggplot(tost.table, aes(x = 0.1, y = d[1])) +
+  geom_hline(yintercept = sesoi) +
+  geom_text(x = -0.35, y = 0.45, label = "Upper bound") +
+  geom_hline(yintercept = -sesoi) +
+  geom_text(x = -0.35, y = -0.45, label = "Lower bound") +
+  geom_errorbar(aes(ymin = d[3], ymax = d[2]), width = 0, size = 1) +
+  geom_point(size = 3) +
+  scale_x_continuous(limits = c(-0.6, 0.5)) +
+  scale_y_continuous(limits = c(-0.6, 0.6)) +
+  labs(y = "Cohen's d",
+       title = "Equivalence testing",
+       subtitle = "Estimated Cohen's d \nagainst our smallest \neffect size of interest (SESOI)") +
+  theme(
+    panel.background = element_rect(fill = "white"),
+    panel.grid = element_line(linetype = "dotted", colour = "gray"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    text = element_text(size = 12, colour = "black"),
+    axis.text = element_text(colour = "black"),
+    axis.text.x = element_blank(),
+    axis.title.x = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  ggsave(here("Figures", "tost.png"), width = 3)
 
 #### power analysis ###################################################
 n <- nrow(data)
@@ -66,12 +94,12 @@ residuals <- residuals(lme4::lmer(time ~ trial_type + (1|participant), data = da
 
 # normality of residuals
 hist(residuals)         # check distribution of residuals
-qqnorm(residuals)       # check residuals' fit to the normal distribution
+qqnorm(residuals)       # check residuals" fit to the normal distribution
 shapiro.test(residuals) # test normality of residuals 
 
 # homocedasticity
 leveneTest(time ~ trial_type, center = mean, data = data)
 
 #### export data #######################################################
-write.table(residuals, 'Data/04_analysis-residuals.txt', sep = '\t', dec = '.')
-write.table(tost.table, 'Data/04_analysis.txt', sep = '\t', dec = '.')
+write.table(residuals, here("Data", "04_analysis-residuals.txt"), sep = "\t", dec = ".")
+write.table(tost.table, here("Data", "04_analysis.txt"), sep = "\t", dec = ".")
